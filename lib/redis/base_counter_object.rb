42410d1893071b355c706cdd1ea203d24bb3e675
Ryosuke Sato <ryz310@gmail.com>
2021-09-19 22:27:21 +0900

    Refactoring

---
+++ lib/redis/base_counter_object.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class Redis
+  class BaseCounterObject < Counter
+    def initialize(key, *args)
+      @original_key = key
+      super(redis_daily_field_key(current_date), *args)
+    end
+
+    attr_reader :original_key
+
+    def [](date, length = nil)
+      if date.is_a? Range
+        range(date.first, date.max)
+      elsif length
+        case length <=> 0
+        when 1  then range(date, next_key(date, length))
+        when 0  then []
+        when -1 then nil  # Ruby does this (a bit weird)
+        end
+      else
+        at(date)
+      end
+    end
+    alias slice []
+
+    def delete(date)
+      redis.del(redis_daily_field_key(date))
+    end
+
+    def range(start_date, end_date)
+      keys = (start_date..end_date).map { |date| redis_daily_field_key(date) }.uniq
+      redis.mget(*keys).map(&:to_i)
+    end
+
+    def at(date)
+      redis.get(redis_daily_field_key(date)).to_i
+    end
+
+    def current_date
+      @current_date ||= (Time.respond_to?(:current) ? Time.current : Time.now).to_date
+    end
+
+    private
+
+    def redis_daily_field_key(_date)
+      raise 'not implemented'
+    end
+
+    def next_key(_date, _length)
+      raise 'not implemented'
+    end
+  end
+end
--- lib/redis/monthly_counter.rb
+++ lib/redis/monthly_counter.rb
@@ -1,48 +1,17 @@
 # frozen_string_literal: true
+require File.dirname(__FILE__) + '/base_counter_object'
 
 class Redis
-  class MonthlyCounter < Counter
-    def initialize(key, *args)
-      @original_key = key
-      @current_date = (Time.respond_to?(:current) ? Time.current : Time.now).to_date
-      super(redis_daily_field_key(current_date), *args)
-    end
-
-    attr_reader :original_key, :current_date
-
-    def [](date, length = nil)
-      if date.is_a? Range
-        range(date.first, date.max)
-      elsif length
-        case length <=> 0
-        when 1  then range(date, date.next_month(length - 1))
-        when 0  then []
-        when -1 then nil  # Ruby does this (a bit weird)
-        end
-      else
-        at(date)
-      end
-    end
-    alias slice []
-
-    def delete(date)
-      redis.del(redis_daily_field_key(date))
-    end
-
-    def range(start_date, end_date)
-      keys = (start_date..end_date).map { |date| redis_daily_field_key(date) }.uniq
-      redis.mget(*keys).map(&:to_i)
-    end
-
-    def at(date)
-      redis.get(redis_daily_field_key(date)).to_i
-    end
-
+  class MonthlyCounter < BaseCounterObject
     private
 
     def redis_daily_field_key(date)
       date_key = date.strftime('%Y-%m')
       [original_key, date_key].flatten.join(':')
+    end
+
+    def next_key(date, length)
+      date.next_month(length - 1)
     end
   end
 end
