bb25d4b651c7c1be99c548b4086306cba371e72e
Ryosuke Sato <ryz310@gmail.com>
2021-09-19 23:22:55 +0900

    Fix offending codes

--- lib/redis/weekly_counter.rb
+++ lib/redis/weekly_counter.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-require File.dirname(__FILE__) + '/base_counter_object'
+
+require "#{File.dirname(__FILE__)}/base_counter_object"
 
 class Redis
   class WeeklyCounter < BaseCounterObject
