d796de6665c624fab60838303d9188738935ca73
Ryosuke Sato <ryz310@gmail.com>
2021-09-19 18:24:30 +0900

    Rename the spec file

--- spec/lib/redis/daily_counter_spec.rb
+++ spec/lib/redis/daily_counter_spec.rb
@@ -1,5 +1,95 @@
 # frozen_string_literal: true
 
 RSpec.describe Redis::DailyCounter do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:mock_class) do
+    Class.new do
+      include Redis::Objects
+
+      daily_counter :my_posts
+
+      def id
+        1
+      end
+    end
+  end
+
+  let(:instance) { MockClass.new }
+
+  before do
+    stub_const 'MockClass', mock_class
+    Timecop.travel(Time.local(2021, 4, 1))
+    instance.my_posts.increment(10)
+    Timecop.travel(Time.local(2021, 4, 2))
+    instance.my_posts.increment(11)
+    Timecop.travel(Time.local(2021, 4, 3))
+    instance.my_posts.increment(12)
+  end
+
+  describe 'keys' do
+    it 'appends new counters automatically with the current date' do
+      expect(instance.redis.get('mock_class:1:my_posts:2021-04-01').to_i).to eq 10
+      expect(instance.redis.get('mock_class:1:my_posts:2021-04-02').to_i).to eq 11
+      expect(instance.redis.get('mock_class:1:my_posts:2021-04-03').to_i).to eq 12
+    end
+  end
+
+  describe '#value' do
+    it 'returns the value counted today' do
+      expect(instance.my_posts.value).to eq 12
+    end
+  end
+
+  describe '#[]' do
+    context 'with date' do
+      let(:date) { Date.new(2021, 4, 1) }
+
+      it 'returns the value counted the day' do
+        expect(instance.my_posts[date]).to eq 10
+      end
+    end
+
+    context 'with date and length' do
+      let(:date) { Date.new(2021, 4, 2) }
+
+      it 'returns the values counted within the duration' do
+        expect(instance.my_posts[date, 2]).to eq [11, 12]
+      end
+    end
+
+    context 'with range' do
+      let(:range) do
+        Date.new(2021, 4, 1)..Date.new(2021, 4, 2)
+      end
+
+      it 'returns the values counted within the duration' do
+        expect(instance.my_posts[range]).to eq [10, 11]
+      end
+    end
+  end
+
+  describe '#delete' do
+    it 'deletes the value on the day' do
+      date = Date.new(2021, 4, 2)
+      expect { instance.my_posts.delete(date) }
+        .to change { instance.my_posts.at(date) }
+        .from(11).to(0)
+    end
+  end
+
+  describe '#range' do
+    let(:start_date) { Date.new(2021, 4, 1) }
+    let(:end_date) { Date.new(2021, 4, 2) }
+
+    it 'returns the values counted within the duration' do
+      expect(instance.my_posts.range(start_date, end_date)).to eq [10, 11]
+    end
+  end
+
+  describe '#at' do
+    let(:date) { Date.new(2021, 4, 2) }
+
+    it 'returns the value counted the day' do
+      expect(instance.my_posts.at(date)).to eq 11
+    end
+  end
 end
